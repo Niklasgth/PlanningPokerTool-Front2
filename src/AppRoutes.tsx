4a9c686e5d75be254b287ee2a42a1ac255e7f737
Maciej Kozendra <ciunio@gmail.com>
2025-05-20 16:30:09 +0200

    Update AppRoutes.tsx

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -13,15 +13,15 @@ const AppRoutes: React.FC = () => {
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/mypage" element={
-
+<ProtectedRoute>
           <MyPage />
-
+</ProtectedRoute>
         } />
 
         <Route path="/pokerpage/:id" element={
-
+<ProtectedRoute>
           <PokerPage />
-
+</ProtectedRoute>
         } />
       </Routes>
     </Router>
